ed029844042f31665dc4b98a71add57bfb15fc3c
juho-kang <juho.kang@webtoonscorp.com>
2020-08-01 16:39:08 +0900

    boot init

---
+++ eat-together/src/main/kotlin/com/man/eattogether/EatTogetherApplication.kt
@@ -0,0 +1,11 @@
+package com.man.eattogether
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EatTogetherApplication
+
+fun main(args: Array<String>) {
+	runApplication<EatTogetherApplication>(*args)
+}
